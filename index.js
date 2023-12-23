ae294e9f8e7461ce1b1a041848f1ed86dd8495ba
Andrea Venti Fuentes <117413846+av1155@users.noreply.github.com>
2023-12-23 16:07:18 -0500

    updated formatter settings (prettier)

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ import { question, questionFloat, questionInt } from "readline-sync";
 function calculateSemesterGPA(semesterCourses) {
     // Calculate the semester GPA
     let convertGPA = new Map();
-    convertGPA.set("A+", 4.00);
-    convertGPA.set("A", 4.00);
-    convertGPA.set("A-", 3.70);
-    convertGPA.set("B+", 3.30);
-    convertGPA.set("B", 3.00);
-    convertGPA.set("B-", 2.70);
-    convertGPA.set("C+", 2.30);
-    convertGPA.set("C", 2.00);
-    convertGPA.set("C-", 1.70);
-    convertGPA.set("D+", 1.30);
-    convertGPA.set("D", 1.00);
-    convertGPA.set("F", 0.00);
+    convertGPA.set("A+", 4.0);
+    convertGPA.set("A", 4.0);
+    convertGPA.set("A-", 3.7);
+    convertGPA.set("B+", 3.3);
+    convertGPA.set("B", 3.0);
+    convertGPA.set("B-", 2.7);
+    convertGPA.set("C+", 2.3);
+    convertGPA.set("C", 2.0);
+    convertGPA.set("C-", 1.7);
+    convertGPA.set("D+", 1.3);
+    convertGPA.set("D", 1.0);
+    convertGPA.set("F", 0.0);
 
     let totalCredits = 0;
     let totalGradePoints = 0;
@@ -32,42 +32,21 @@ function calculateSemesterGPA(semesterCourses) {
     return semesterGpa.toFixed(2);
 }
 
-function calculateOverallGPA(
-    totalGPA,
-    totalCredits,
-    semesterGPA,
-    semesterCredits,
-) {
+function calculateOverallGPA(totalGPA, totalCredits, semesterGPA, semesterCredits) {
     // Calculate the updated overall GPA
     let updatedOverallGPA =
-        ((totalGPA * totalCredits) + (semesterGPA * semesterCredits)) /
-        (totalCredits + semesterCredits);
+        (totalGPA * totalCredits + semesterGPA * semesterCredits) / (totalCredits + semesterCredits);
 
     return updatedOverallGPA.toFixed(2);
 }
 
 async function main() {
     // logic to interact with user and invoke calculations
-    let numCourses = questionInt(
-        "How many courses are you taking? ",
-    );
+    let numCourses = questionInt("How many courses are you taking? ");
     console.log(); // New line
 
     let semesterCourses = [];
-    let possibleGrades = [
-        "A+",
-        "A",
-        "A-",
-        "B+",
-        "B",
-        "B-",
-        "C+",
-        "C",
-        "C-",
-        "D+",
-        "D",
-        "F",
-    ];
+    let possibleGrades = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "F"];
 
     for (let i = 0; i < numCourses; i++) {
         console.log("Course " + (i + 1) + ":");
@@ -87,9 +66,7 @@ async function main() {
         let courseCredits = -1;
         while (courseCredits < 0 || isNaN(courseCredits)) {
             if (courseCredits !== -1) {
-                console.log(
-                    "Invalid input. Please enter a positive number of credits.",
-                );
+                console.log("Invalid input. Please enter a positive number of credits.");
             }
             courseCredits = questionInt("Enter course credits: ");
         }
@@ -105,9 +82,7 @@ async function main() {
 
     let semesterCredits = 0;
     semesterCourses.forEach((course) => {
-        console.log(
-            `Course: ${course.name}, Grade: ${course.grade}, Credits: ${course.credits}`,
-        );
+        console.log(`Course: ${course.name}, Grade: ${course.grade}, Credits: ${course.credits}`);
         semesterCredits += course.credits;
     });
 
@@ -119,30 +94,23 @@ async function main() {
     let totalGPA = -1;
     let totalCredits = -1;
     if (decision.toUpperCase() === "Y" || decision.toUpperCase() === "YES") {
-        while (totalGPA < 0.00 || totalGPA > 4.00 || isNaN(totalGPA)) {
-            if (totalGPA !== -1) { // This check prevents the message from showing the first time
+        while (totalGPA < 0.0 || totalGPA > 4.0 || isNaN(totalGPA)) {
+            if (totalGPA !== -1) {
+                // This check prevents the message from showing the first time
                 console.log("Invalid input. Please enter a GPA between 0.00 and 4.00.");
             }
             totalGPA = questionFloat("Current overall GPA: ");
         }
         while (totalCredits < 0 || isNaN(totalCredits)) {
-            if (totalCredits !== -1) { // Similarly, this prevents the message from showing the first time
-                console.log(
-                    "Invalid input. Please enter a positive number of credits.",
-                );
+            if (totalCredits !== -1) {
+                // Similarly, this prevents the message from showing the first time
+                console.log("Invalid input. Please enter a positive number of credits.");
             }
             totalCredits = questionInt("Total credits earned: ");
         }
 
-        console.log(
-            "Overall GPA: " +
-            calculateOverallGPA(
-                totalGPA,
-                totalCredits,
-                semesterGPA,
-                semesterCredits,
-            ),
-        );
+        console.log("Overall GPA: " + calculateOverallGPA(totalGPA, totalCredits, semesterGPA, semesterCredits));
+        console.log("Total credits earned: " + (totalCredits + semesterCredits));
     } else {
         console.log("Goodbye!");
     }
